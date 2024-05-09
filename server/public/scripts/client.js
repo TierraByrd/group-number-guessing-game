009dbdd846775fd5989f0570774a2d3b5e428852
Tristan Bridges <mophead4@gmail.com>
2024-05-09 14:29:32 -0500

    functional

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,58 @@
+
+
 function onReady() {
   console.log("JavaScript is loaded!")
 }
+
+
+function submitGuess(event){
+  event.preventDefault()
+  console.log('submit works')
+
+
+  let guessOne = document.getElementById('guessOne').value
+  let guessTwo = document.getElementById('guessTwo').value
+
+  
+  
+  console.log(guessOne, guessTwo)
+  axios({
+    method: 'POST',
+    url: '/guess',
+    data: {
+      first: guessOne,
+      second: guessTwo
+    }
+  })
+  .then((response)=>{
+    console.log('success')
+    renderResults()
+  })
+
+
+}
+
+function renderResults(){
+  let renderDiv = document.getElementById('resultsDiv')
+
+  axios({
+    method: 'GET',
+    url: '/guess'
+  })
+    .then((response)=>{
+      console.log(response)
+
+      let guessData = response.data
+      console.log(guessData)
+      renderDiv.innerHTML = ''
+      for (let guess of guessData){
+      renderDiv.innerHTML += `
+      <div> Player 1 Guess: ${guess.firstPlayerGuess} Results: ${guess.playerOneResults} </div>
+      <div> Player 2 Guess: ${guess.secondPlayerGuess} Results: ${guess.playerTwoResults} </div>`
+      }
+
+    })
+}
+
 
 onReady()
--- server/server.js
+++ server/server.js
@@ -1,17 +1,73 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 5000;
 
 // This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}))
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static files (HTML, CSS, Client JS)
-app.use(express.static('server/public'));
+app.use(express.static("server/public"));
 
 // GET & POST Routes go here
+let randomNumber = Math.random() * (25 - 1) + 1;
+let winningNumber = randomNumber.toFixed(0);
+let guesses = [];
+console.log(winningNumber)
 
+function checkGuess(guessInput) {
+  let roundGuesses = {};
+  roundGuesses.firstPlayerGuess = guessInput.first;
+  roundGuesses.secondPlayerGuess = guessInput.second;
+
+  if (guessInput.first == winningNumber) {
+    roundGuesses.playerOneResults = "Winner";
+  } else if (Number(guessInput.first) > winningNumber) {
+    roundGuesses.playerOneResults = "No dice, too high";
+  } else if (Number(guessInput.first) < winningNumber) {
+    roundGuesses.playerOneResults = "Too low, try again";
+  }
+  if (guessInput.second == winningNumber) {
+    roundGuesses.playerTwoResults = "Winner";
+  } else if (Number(guessInput.second) > winningNumber) {
+    roundGuesses.playerTwoResults = "No dice, too high";
+  } else if (Number(guessInput.second) < winningNumber) {
+    roundGuesses.playerTwoResults = "Too low, try again";
+  }
+  guesses.push(roundGuesses);
+}
+
+app.get("/random", (req, res) => {
+  let randomNumber = Math.random() * (25 - 1) + 1;
+  let winningNumber = randomNumber.toFixed(0);
+
+  res.send(winningNumber)
+});
+
+app.get("/guess", (req, res) => {
+  res.send(guesses);
+});
+
+app.post("/guess", (req, res) => {
+  let guessInput = req.body;
+
+  console.log("Got a POST request", guessInput);
+  console.log("array guesses", guesses);
+
+  checkGuess(guessInput);
+
+  console.log("array guesses after pushing roundGuesses", guesses);
+
+  res.sendStatus(201);
+});
+
+// app.get('/random', (req, res)=>){
+//   console.log('in/random')
+//   res.send(randomData)
+// }
 
 app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
-})
+  console.log("Server is running on port", PORT);
+});
